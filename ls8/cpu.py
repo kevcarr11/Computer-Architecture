293f9c1502b8435c19c7db5146ef0c5915004d63
Kevin <kevincarr632@gmail.com>
2020-09-06 17:19:15 -0500

    extended emulator to support mult.ls8 program

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -26,24 +26,6 @@ class CPU:
     def load(self):
         """
         Load a program into memory.
-
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
         """
         address = 0
 
@@ -75,7 +57,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[0] = reg_a * reg_b
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -113,6 +96,9 @@ class CPU:
                 self.reg[operand_a] = operand_b
                 self.pc += 3
 
+            elif instruction == self.opcodes["MUL"]:
+                self.alu("MUL", self.reg[0], self.reg[1])
+                self.pc += 3
 
             elif instruction == self.opcodes["PRN"]:
                 reg_locaton = self.ram_read(self.pc + 1)
